7ba02cf8d3cc76f773923193eb7f4a775f5bc1e4
chetan-cv <chetanverma99.cv@gmail.com>
2021-04-16 18:36:11 +0530

    updaitng

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,12 @@ import "firebase/auth";
 import "firebase/functions";
 
 firebase.initializeApp({
-// Paste here your config file
+    apiKey: "AIzaSyD9hSjlY6OcedTzl4YC1ImQyumXneTMPzg",
+    authDomain: "fir-react-12354.firebaseapp.com",
+    projectId: "fir-react-12354",
+    storageBucket: "fir-react-12354.appspot.com",
+    messagingSenderId: "449164814025",
+    appId: "1:449164814025:web:cb6ee9319faf4102858f18"
 });
 
 export const auth = firebase.auth();
